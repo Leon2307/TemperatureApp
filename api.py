e89266529689a009a6753ac94348cda48fb9fc2f
Leon <leonfischer20031@web.de>
2024-01-30 09:24:51 +0200

    Adds C file

--- api.py
+++ api.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('temperature_page.html')
+    return render_template('index.html')
+
+@app.get('/test')
+def test():
+    return "{'hallo': 123}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0')
--- database.py
+++ database.py
@@ -9,12 +9,7 @@ def setup():
     cur = con.cursor()
     cur.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ( tempID PRIMARY KEY AUTO_INCREMENT, value INT, time NUMERIC )')
     con.commit()
-    con.close()
 
-def drop_db():
-    con = sqlite3.connect(DB_NAME)
-    cur = con.cursor()
-    cur.execute(f'DROP TABLE {TABLE_NAME}')
 
 if __name__ == '__main__':
     setup()
