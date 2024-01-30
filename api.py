9112060179a2833196b61e4169737a3234d9f3d2
Leon <leonfischer20031@web.de>
2024-01-30 19:36:22 +0200

    Syntax fixes in api.py

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ def current_temp():
     location = request.args.get("location")
     temp_degree = get_current_temp(location)
     temp_fahrenheit = 1.8 * temp_degree + 3
-    temp_json = f'\{"temperature_degree":{temp_degree}, "temperature_fahrenheit":{temp_fahrenheit}\}'
+    temp_json = f'{{"temperature_degree":{temp_degree}, "temperature_fahrenheit":{temp_fahrenheit}}}'
     return temp_json
 
 
@@ -25,26 +25,29 @@ def current_temp():
 def max_temp():
     location = request.args.get("location")
     max_temp_degree = get_max_temp(location)
-    max_temp_fahrenheit = 1.8 * max_temp_degree +32
-    max_temp_json = f'\{"temperature_degree":{max_temp_degree}, "temperature_fahrenheit":{max_temp_fahrenheit}\}'
+    max_temp_fahrenheit = 1.8 * max_temp_degree + 32
+    max_temp_json = f'{{"temperature_degree":{max_temp_degree}, "temperature_fahrenheit":{max_temp_fahrenheit}}}'
     return max_temp_json
+
 
 # Get min temperature (e.g. /minTemperature?location="Mikkeli")
 @app.get("/minTemperature")
 def min_temp():
     location = request.args.get("location")
     min_temp_degree = get_min_temp(location)
-    min_temp_fahrenheit = 1.8 * min_temp_degree +32
-    min_temp_json = f'\{"temperature_degree":{min_temp_degree}, "temperature_fahrenheit":{min_temp_fahrenheit}\}'
+    min_temp_fahrenheit = 1.8 * min_temp_degree + 32
+    min_temp_json = f'{{"temperature_degree":{min_temp_degree}, "temperature_fahrenheit":{min_temp_fahrenheit}}}'
     return min_temp_json
+
 
 # Get current humidity (e.g. /currentHumidity?location="Mikkeli")
 @app.get("/currentHumidity")
 def current_humidity():
     location = request.args.get("location")
     current_humidity = get_current_humidity(location)
-    current_humidity_json = f'\{"current_humidity":{current_humidity}\}'
+    current_humidity_json = f'{{"current_humidity":{current_humidity}}}'
     return current_humidity_json
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
