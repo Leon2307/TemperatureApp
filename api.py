1bd9e14e54ef6c53339a3eae87c4cc1ae368909f
Leon <leonfischer20031@web.de>
2024-01-30 10:02:58 +0200

    current temperature function added

--- api.py
+++ api.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import get_current_temp
 
 app = Flask(__name__)
 
-@app.route('/')
+
+# Get Website
+@app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.get('/test')
-def test():
-    return "{'hallo': 123}"
 
-if __name__ == '__main__':
-    app.run('0.0.0.0')
+# Get current temperature (e.g. /currentTemperature?location="Mikkeli")
+@app.get("/currentTemperature")
+def current_temp():
+    location = request.args.get("location")
+    temp_degree = get_current_temp(location)
+    temp_fahrenheit = 1.8 * temp_degree + 32
+    temp_json = f'"temperature_degree":{temp_degree}, "temperature_fahrenheit":{temp_fahrenheit}'
+    return temp_json
+
+
+# Get max temperature
+
+
+# Get min temperature
+
+# Get current humidity
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
