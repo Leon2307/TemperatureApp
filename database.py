55c9f6b57d982da8f67f025c149f0f80d3d4b673
Schoeck <lukas.schoeck@sap.com>
2024-01-31 12:26:05 +0200

    execute_with_connection added

--- database.py
+++ database.py
@@ -5,10 +5,33 @@ TABLE_NAME = "temperature"
 
 # Create Database and Table
 def setup():
-    con = sqlite3.connect(DB_NAME)
+    execute_with_connection(
+        f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ( id INTEGER PRIMARY KEY AUTOINCREMENT, degree REAL, humidity REAL, timestamp NUMERIC, location TEXT )')
+
+
+def execute_with_connection(query, attributes=None):
+    try:
+        con = sqlite3.connect(DB_NAME)
+    except:
+        print("Connection to DB couldn't be established.")
     cur = con.cursor()
-    cur.execute(f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ( tempID PRIMARY KEY AUTO_INCREMENT, value INT, time NUMERIC )')
+    if attributes is None:
+        cur.execute(query)
+    else:
+        cur.execute(query, attributes)
     con.commit()
+    con.close()
+
+# def get_max_temp(Location=None):​
+
+# def get_min_temp(Location=None):​
+
+# def get_latest_temp(Location):​
+
+# def get_latest_humidity(Location):​
+
+def insert_values(degree, humidity, location):
+    
 
 
 if __name__ == '__main__':
