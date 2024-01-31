7d1c4f192c77a428cae758dea32166bf9664065f
lukassck <lukas.schoeck@web.de>
2024-01-31 14:05:39 +0200

    functions added but to be checked

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 DB_NAME = "temperatureApp.db"
 TABLE_NAME = "temperature"
@@ -6,10 +7,11 @@ TABLE_NAME = "temperature"
 # Create Database and Table
 def setup():
     execute_with_connection(
-        f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ( id INTEGER PRIMARY KEY AUTOINCREMENT, degree REAL, humidity REAL, timestamp NUMERIC, location TEXT )')
+        f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ( id INTEGER PRIMARY KEY AUTOINCREMENT, degree REAL, humidity REAL, timestamp NUMERIC, location TEXT )'
+    )
 
 
-def execute_with_connection(query, attributes=None):
+def execute_with_connection(query, attributes = None):
     try:
         con = sqlite3.connect(DB_NAME)
     except:
@@ -17,24 +19,62 @@ def execute_with_connection(query, attributes=None):
     cur = con.cursor()
     if attributes is None:
         cur.execute(query)
+        for x in cur.fetchall():
+            print(x)
+
     else:
         cur.execute(query, attributes)
     con.commit()
     con.close()
 
-# def get_max_temp(Location=None):​
-
-# def get_min_temp(Location=None):​
-
-# def get_latest_temp(Location):​
-
-# def get_latest_humidity(Location):​
-
 def insert_values(degree, humidity, location):
-    
+    time_raw = datetime.datetime.now()
+    timestamp = time_raw.strftime("%Y%m%d%H%M%S")
+    execute_with_connection(
+        f'INSERT INTO {TABLE_NAME} (degree, humidity, location, timestamp) VALUES (?, ?, ?, ?)', (degree, humidity, location, timestamp)
+    )
 
+def get_max_temp(location):
+    if location == None:
+        execute_with_connection(
+            f'SELECT MAX(degree) FROM {TABLE_NAME} GROUP BY degree'
+        )
+    else:
+        execute_with_connection(
+            f'SELECT MAX(degree) FROM {TABLE_NAME} WHERE location = ?', (location,)
+        )
+
+def get_min_temp(location):
+    if location == None:
+        execute_with_connection(
+            f'SELECT MIN(degree) FROM {TABLE_NAME} GROUP BY degree'
+        )
+    else:
+        execute_with_connection(
+            f'SELECT MIN(degree) FROM {TABLE_NAME} WHERE location = ?', (location,)
+        )
+
+def get_latest_temp(location):
+    if location == None:
+        execute_with_connection(
+            f'SELECT degree FROM {TABLE_NAME}'
+        )
+    else:
+        execute_with_connection(
+            f'SELECT degree FROM {TABLE_NAME} WHERE location = ? HAVING MAX(timestamp)', (location,)
+        )
+
+def get_latest_humidity(location):
+    if location == None:
+        execute_with_connection(
+            f'SELECT .humidity FROM {TABLE_NAME}'
+        )
+    else:
+        execute_with_connection(
+            f'SELECT humidity FROM {TABLE_NAME} WHERE location = ? HAVING MAX(timestamp)', (location,)
+        )
 
 if __name__ == '__main__':
     setup()
-
-
+    get_max_temp('abc')
+    # execute_with_connection(f'SELECT * FROM {TABLE_NAME}')
