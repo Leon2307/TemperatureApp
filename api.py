27bbbce8518b9e65dcc608c3b2514f1d8e6c6429
Leon <leonfischer20031@web.de>
2024-01-31 20:46:52 +0200

    Fixes database problems and exception handling

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from database import get_current_temp, get_max_temp, get_min_temp, get_current_humidity
+from database import get_latest_temp, get_max_temp, get_min_temp, get_latest_humidity
 
 app = Flask(__name__)
 
@@ -10,41 +10,53 @@ def main():
     return render_template("index.html")
 
 
-# Get current temperature (e.g. /currentTemperature?location="Mikkeli")
+# Get current temperature (e.g. /currentTemperature?location=Mikkeli)
 @app.get("/currentTemperature")
 def current_temp():
     location = request.args.get("location")
-    temp_degree = get_current_temp(location)
-    temp_fahrenheit = 1.8 * temp_degree + 3
+    latest_temp = get_latest_temp(location)
+    if latest_temp is None:
+        return '{"temperature_degree": null, "temperature_fahrenheit": null}'
+    temp_degree = float(latest_temp)
+    temp_fahrenheit = 1.8 * temp_degree + 3.0
     temp_json = f'{{"temperature_degree":{temp_degree}, "temperature_fahrenheit":{temp_fahrenheit}}}'
     return temp_json
 
 
-# Get max temperature (e.g. /maxTemperature?location="Mikkeli")
+# Get max temperature (e.g. /maxTemperature?location=Mikkeli)
 @app.get("/maxTemperature")
 def max_temp():
     location = request.args.get("location")
-    max_temp_degree = get_max_temp(location)
+    max_temp = get_max_temp(location)
+    if max_temp is None:
+        return '{"temperature_degree": null, "temperature_fahrenheit": null}'
+    max_temp_degree = float(max_temp)
     max_temp_fahrenheit = 1.8 * max_temp_degree + 32
     max_temp_json = f'{{"temperature_degree":{max_temp_degree}, "temperature_fahrenheit":{max_temp_fahrenheit}}}'
     return max_temp_json
 
 
-# Get min temperature (e.g. /minTemperature?location="Mikkeli")
+# Get min temperature (e.g. /minTemperature?location=Mikkeli)
 @app.get("/minTemperature")
 def min_temp():
     location = request.args.get("location")
-    min_temp_degree = get_min_temp(location)
+    min_temp = get_min_temp(location)
+    if min_temp is None:
+        return '{"temperature_degree": null, "temperature_fahrenheit": null}'
+    min_temp_degree = float(min_temp)
     min_temp_fahrenheit = 1.8 * min_temp_degree + 32
     min_temp_json = f'{{"temperature_degree":{min_temp_degree}, "temperature_fahrenheit":{min_temp_fahrenheit}}}'
     return min_temp_json
 
 
-# Get current humidity (e.g. /currentHumidity?location="Mikkeli")
+# Get current humidity (e.g. /currentHumidity?location=Mikkeli)
 @app.get("/currentHumidity")
 def current_humidity():
     location = request.args.get("location")
-    current_humidity = get_current_humidity(location)
+    latest_humidity = get_latest_humidity(location)
+    if latest_humidity is None:
+        return '{"temperature_degree": null, "temperature_fahrenheit": null}'
+    current_humidity = int(latest_humidity)
     current_humidity_json = f'{{"current_humidity":{current_humidity}}}'
     return current_humidity_json
 
--- database.py
+++ database.py
@@ -4,72 +4,102 @@ import datetime
 DB_NAME = "temperatureApp.db"
 TABLE_NAME = "temperature"
 
+
 def setup():
     execute_with_connection(
-        f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ( id INTEGER PRIMARY KEY AUTOINCREMENT, degree REAL, humidity REAL, timestamp NUMERIC, location TEXT )'
+        f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} ( id INTEGER PRIMARY KEY AUTOINCREMENT, degree REAL, humidity REAL, timestamp NUMERIC, location TEXT )"
     )
+
 
 def execute_with_connection(query, attributes=None):
     try:
         con = sqlite3.connect(DB_NAME)
-    except:
+    except sqlite3.Error:
         print("Connection to DB couldn't be established.")
+        return None
     cur = con.cursor()
-    if attributes is None:
-        cur.execute(query)
-    else:
-        cur.execute(query, attributes)
-    for x in cur.fetchall():
-            print(x)
+    try:
+        if attributes is None:
+            cur.execute(query)
+        else:
+            cur.execute(query, attributes)
+        result = cur.fetchall()
+    except sqlite3.OperationalError:
+        print("Query could not be executed")
+        return None
     con.commit()
     con.close()
+    if result == []:
+        result = None 
+    return result
+
 
 def insert_values(degree, humidity, location):
     time_raw = datetime.datetime.now()
     timestamp = time_raw.strftime("%Y%m%d%H%M%S")
     execute_with_connection(
-        f'INSERT INTO {TABLE_NAME} (degree, humidity, location, timestamp) VALUES (?, ?, ?, ?)', (degree, humidity, location, timestamp)
+        f"INSERT INTO {TABLE_NAME} (degree, humidity, location, timestamp) VALUES (?, ?, ?, ?)",
+        (degree, humidity, location, timestamp),
     )
 
+
 def get_max_temp(location):
-    if location == None:
-        execute_with_connection(
-            f'SELECT MAX(degree) FROM {TABLE_NAME} GROUP BY degree'
-        )
+    result = None
+    if location is None:
+        result = execute_with_connection(f"SELECT MAX(degree) FROM {TABLE_NAME}")
     else:
-        execute_with_connection(
-            f'SELECT MAX(degree) FROM {TABLE_NAME} WHERE location = ?', (location,)
+        result = execute_with_connection(
+            f"SELECT MAX(degree) FROM {TABLE_NAME} WHERE location = ?", (location,)
         )
+    if result is None:
+        return None
+    return result[0][0]
+
 
 def get_min_temp(location):
-    if location == None:
-        execute_with_connection(
-            f'SELECT MIN(degree) FROM {TABLE_NAME} GROUP BY degree'
-        )
+    result = None
+    if location is None:
+        result = execute_with_connection(f"SELECT MIN(degree) FROM {TABLE_NAME}")
     else:
-        execute_with_connection(
-            f'SELECT MIN(degree) FROM {TABLE_NAME} WHERE location = ?', (location,)
+        result = execute_with_connection(
+            f"SELECT MIN(degree) FROM {TABLE_NAME} WHERE location = ?", (location,)
         )
+    if result is None:
+        return None
+    return result[0][0]
+
 
 def get_latest_temp(location):
-    if location == None:
-        execute_with_connection(
-            f'SELECT degree FROM {TABLE_NAME}'
+    result = None
+    if location is None:
+        result = execute_with_connection(
+            f"SELECT degree FROM {TABLE_NAME} ORDER BY timestamp DESC LIMIT 1"
         )
     else:
-        execute_with_connection(
-            f'SELECT degree FROM {TABLE_NAME} WHERE location = ? GROUP BY location HAVING MAX(timestamp)', (location,)
+        result = execute_with_connection(
+            f"SELECT degree FROM {TABLE_NAME} WHERE location = ? ORDER BY timestamp DESC LIMIT 1",
+            (location,),
         )
+    if result is None:
+        return None
+    return result[0][0]
+
 
 def get_latest_humidity(location):
-    if location == None:
-        execute_with_connection(
-            f'SELECT .humidity FROM {TABLE_NAME}'
+    result = None
+    if location is None:
+        result = execute_with_connection(
+            f"SELECT humidity FROM {TABLE_NAME} ORDER BY timestamp DESC LIMIT 1"
         )
     else:
-        execute_with_connection(
-            f'SELECT humidity FROM {TABLE_NAME} WHERE location = ? GROUP BY location HAVING MAX(timestamp)', (location,)
+        result = execute_with_connection(
+            f"SELECT humidity FROM {TABLE_NAME} WHERE location = ? ORDER BY timestamp DESC LIMIT 1",
+            (location,),
         )
+    if result is None:
+        return None
+    return result[0][0]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     setup()
